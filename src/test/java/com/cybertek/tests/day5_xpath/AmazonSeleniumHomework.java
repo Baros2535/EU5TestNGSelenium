f816fb3879b78018f03dac52d0ed2414bba72ec3
Baros2535 <brsaltn.25@gmail.com>
2021-06-09 03:19:18 +0300

    09 JUNE 2021 03:20

---
+++ src/test/java/com/cybertek/tests/day5_xpath/AmazonSeleniumHomework.java
@@ -0,0 +1,31 @@
+package com.cybertek.tests.day5_xpath;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class AmazonSeleniumHomework {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.get("http://amazon.com");
+        WebElement searchTextBox=driver.findElement(By.xpath("(//div/input)[4]"));
+        String searched="selenium";
+        searchTextBox.sendKeys(searched);
+        WebElement searchButton= driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]"));
+        searchButton.click();
+        WebElement locationOfText=driver.findElement(By.xpath("(//span[@dir=\"auto\"])[1]"));
+        String actualResult= locationOfText.getText() + " \"" + searched + "\"";
+        System.out.println(actualResult);
+        String expectedResult="1-48 of 150 results for \"selenium\"";
+        if (expectedResult.equals(actualResult)){
+            System.out.println("PASS");
+
+        }else{
+            System.out.println("FAİL");
+        }
+        Thread.sleep(2000);
+driver.quit();
+
+    }
+}
